f4b002bdb28619f0e6eb1f22f944c022df70e93f
Tim Hall <timothypatrickhall@gmail.com>
2015-07-24 20:38:30 +0000

    Added alert to user.js to test functionality on heroku

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    alert("Yo!");
     Stripe.setPublishableKey($('meta[name= "stripe-key"]').attr('content'));
     //Watch for a form submission
     $("#form-submit-btn").click(function(event) {
